c50f02d9aea36d64798d4583776cf456055c435f
volaticus <>
2023-10-15 20:21:55 +0300

    install ohmyzsh

--- install.sh
+++ install.sh
@@ -4,11 +4,6 @@
 set -euo pipefail
 
 echo "start installing..."
-echo "enter your name:" 
-read myName
-echo "enter your email:" 
-read myEmail
-
 mkdir -p ~/.install
 
 pacmanPkgs=(
@@ -21,12 +16,6 @@ pacmanPkgs=(
 	"neofetch"
 )
 sudo pacman -S --noconfirm --needed "${pacmanPkgs[@]}"
-git config --global init.defaultBranch main
-git config --global user.email "$myEmail"
-git config --global user.name "$myName"
-  
-chsh -s $(which zsh)
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 if sudo pacman -Qs yay > /dev/null; then
 	echo "yay is installed. skipping..."
@@ -40,7 +29,27 @@ fi
 
 yayPkgs=(
 	"google-chrome"
+	"git-credential-manager-core"
 )
 yay -S --noconfirm --needed --answerdiff None --answerclean None "${yayPkgs[@]}"
+
+git config --global init.defaultBranch main
+git config --global user.email ""
+git config --global user.name "volaticus"
+git config --global credential.credentialStore cache
+git config --global credential.cacheOptions "--timeout 7200"  
+
+if [[ $SHELL != "/usr/bin/zsh" ]]; then
+	chsh -s $(which zsh)
+else
+	echo "shell not changed, already set to ZSH"
+fi
+
+if [ ! -d ~/.oh-my-zsh ]; then
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+else
+	echo "skip installing ohmyzsh because ~/.oh-my-zsh folder already exists"
+fi
+
 
 
